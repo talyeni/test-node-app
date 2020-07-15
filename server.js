c17f89a164c0cc78d8372602d0525e24a0906cac
oussema mkawar <osamamkawar@gmail.com>
2020-07-15 02:49:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile('index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(PORT, HOST, () => {
